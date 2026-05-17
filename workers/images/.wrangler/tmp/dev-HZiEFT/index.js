69a5082e9ce6b0def51a2b648fb687e311cbe04c
rianjordan <rianjordan@users.noreply.github.com>
2026-05-18 01:30:18 +0700

    fix: remove unowned custom routes to allow free wrangler subdomain deployment

--- workers/images/.wrangler/tmp/dev-HZiEFT/index.js
+++ workers/images/.wrangler/tmp/dev-HZiEFT/index.js
@@ -122,7 +122,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-JeV3qN/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-htUo67/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -154,7 +154,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-JeV3qN/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-htUo67/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
